339efa6eab79155ddbf227cefdd094d0618a3fa6
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-03-06 21:44:08 +0100

    adding validation rules v0

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -16,4 +16,15 @@ class RegisterModel extends Model
     {
         echo "user Created !";
     }
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 255]],
+            'repeatPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+
+        ];
+    }
 }
